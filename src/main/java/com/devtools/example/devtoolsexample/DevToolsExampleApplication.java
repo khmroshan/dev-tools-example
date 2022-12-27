f9d92311ad013a6c870774a9bb66b1ff23274308
roshan <roshan.khambu@coac.de>
2022-12-27 13:38:01 +0545

    init push

---
+++ src/main/java/com/devtools/example/devtoolsexample/DevToolsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.devtools.example.devtoolsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevToolsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devtools/example/devtoolsexample/DevToolsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtools.example.devtoolsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevToolsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
